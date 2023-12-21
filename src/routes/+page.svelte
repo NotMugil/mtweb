1ec1d31ac91c0563fcd20022be340cf2c68bca53
suslime <mugilsubramani@gmail.com>
2023-12-21 20:31:40 +0530

    Added Header

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="content-center items-center flex flex-col flex-nowrap justify-start max-w-screen-sm mx-auto lg:gap-16">
+    <!--Static-->
+    
+    <!--Header-->
+    <div class="justify-center text-center mt-16 lg:mt-20">
+        <div class="py-2 lg:py-4">
+            <img src="https://avatars.githubusercontent.com/u/86902446?v=4" alt="Mugilan S" class="rounded-full h-20 w-20 mx-auto lg:w-24 lg:h-24">
+        </div>
+        <h1 class="text-white text-3xl font-semibold font-pop py-4 lg:text-4xl lg:py-4">Mugilan</h1>
+        <p class="text-white font-pop font-normal lg:text-base max-w-sm mx-auto">Transforming ideas into digital experiences. Passionate about creating. </p>
+    </div>
+
+    <!--Content-->
+
+</div>
